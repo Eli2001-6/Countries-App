10942927ac67d8d83897e54a2c7c4d391035d750
Elizabeth UWAMAHORO <uwamahoroelisa@gmail.com>
2023-08-05 15:39:33 +0200

    commit changes

---
+++ script.js
@@ -0,0 +1,98 @@
+
+window.addEventListener('load', requestCountryData);
+
+function requestCountryData() {
+    
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://restcountries.com/v2/all', true);
+
+    xhr.onload = function () {
+        if(xhr.status == 200) { 
+            let countries = JSON.parse(this.response);
+            data = countries.map(country => {
+                // for country template display 
+                const countryCardContainer =  document.querySelector('[country-card-container]');
+                const countryTemplate = document.querySelector('[country-template]');
+                const countryCard = countryTemplate.content.cloneNode(true).children[0];
+
+                const countryNames = countryCard.querySelector('[country-name]');
+                const countryCapital = countryCard.querySelector('[country-capital]');
+                const countryFlag = countryCard.querySelector('[country-flag]');
+                const countryPopulation = countryCard.querySelector('[country-population]');
+                const countryRegion = countryCard.querySelector('[country-region]');
+                
+                countryFlag.src = country.flags.png;
+                countryPopulation.textContent = country.population;
+                countryNames.textContent = country.name;
+                countryRegion.textContent = country.region;
+                countryCapital.textContent = country.capital;
+
+                countryCardContainer.appendChild(countryCard);
+                    
+                return {
+                    name: country.name,
+                    population: country.population,
+                    region: country.region,
+                    capital: country.capital,
+                    flags: country.flags.png,
+                    element: countryCard
+                }
+            })
+        }
+    }
+    xhr.send();
+}
+const colorMode  = document.body;
+const headerMode = document.getElementById('header');
+const searchBtn = document.getElementById('searchBtn');
+const searchImg = document.getElementById('searchImg');
+const searchFieldMode = document.getElementById('searchBar');
+const regionFilter = document.getElementById('regionFilter');
+const colorModeBtn = document.querySelector('.fa-regular');
+const colorToggle = document.querySelector('.color-toggle');
+
+colorModeBtn.addEventListener('click', colorChange);
+
+function colorChange() {
+    colorMode.classList.toggle('dark-mode');
+    headerMode.classList.toggle('header-dark');
+    searchFieldMode.classList.toggle('search-field-dark');
+    searchBtn.classList.toggle('search-btn-dark');
+    searchImg.classList.toggle('fa-magnifying-glass-dark');
+    regionFilter.classList.toggle('region-filter-dark');
+    
+    if (colorMode.classList.contains('dark-mode')) {
+        colorToggle.textContent = ' Light Mode';
+    } else {
+        colorToggle.textContent = ' Dark Mode';
+    }
+}
+
+
+let data = [];
+const searchInput = document.querySelector('[data-search]');
+searchInput.addEventListener('input', (e) => {
+    const value = e.target.value.toLowerCase();
+    data.forEach(country => {
+        const isVisible = 
+        country.name.toLowerCase().includes(value);
+        country.element.classList.toggle('hide-card', !isVisible);
+    })
+});
+const regionInput = document.querySelector('[region-input]');
+regionInput.addEventListener('change', () => {
+    const regionFilterValue = document.getElementById('regionFilter').value;
+    
+    data.forEach(country => {
+        const isVisible = 
+        country.region.toLowerCase().includes(regionFilterValue);
+        country.element.classList.toggle('hide-card',!isVisible);
+
+        if (regionFilterValue === "all") {
+            country.element.classList.remove('hide-card');
+        } else {
+            isVisible;
+        }
+    })
+});
